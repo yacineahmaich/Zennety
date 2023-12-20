b688cb33817b734af32bf202375c9cafe826b85c
yacineahmaich <yacineahmaich@gmail.com>
2023-12-20 23:59:59 +0100

    chore [Frontend]: Reload i18n on rerender

--- www/next-i18next.config.js
+++ www/next-i18next.config.js
@@ -1,10 +1,11 @@
-const path = require('path');
+const path = require("path");
 
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
-  localePath: path.resolve('./locales'),
+  reloadOnPrerender: process.env.ENV === "dev",
+  localePath: path.resolve("./locales"),
 };
