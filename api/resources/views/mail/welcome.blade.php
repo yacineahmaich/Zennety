52ed9725f8b6a48c21a8041d6a6a6ec245b87eb5
yacineahmaich <yacineahmaich@gmail.com>
2023-12-21 21:29:27 +0100

    feat [Backend]: setup email sending & welcome email

---
+++ api/resources/views/mail/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
+    xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+    <!--[if (gte mso 9)|(IE)]>
+    <xml>
+        <o:OfficeDocumentSettings>
+            <o:AllowPNG/>
+            <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+    </xml>
+    <![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- So that mobile will display zoomed in -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- enable media queries for windows phone 8 -->
+    <meta name="format-detection" content="telephone=no"> <!-- disable auto telephone linking in iOS -->
+    <meta name="format-detection" content="date=no"> <!-- disable auto date linking in iOS -->
+    <meta name="format-detection" content="address=no"> <!-- disable auto address linking in iOS -->
+    <meta name="format-detection" content="email=no"> <!-- disable auto email linking in iOS -->
+    <meta name="author" content="Simple-Pleb.com">
+    <title>Welcome | {{ config('app.name') }}</title>
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+</head>
+
+<body>
+
+    <h1>Hi, {{$user->name}}</h1>
+
+    Welcome to {{config('app.name')}}
+</body>
+
+</html>
