61db7224a88dd4fe80d90408c9b66894dc2a65c6
yacineahmaich <yacineahmaich@gmail.com>
2023-12-29 23:54:06 +0100

    chore [Backend]: make the users search endpoint standard

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -4,17 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function search($search)
     {
         $users = User::query()
-            ->where('id', '!=', auth()->id())
             ->where(function ($query) use ($search) {
-                $query->where('name', 'like', '%' . $search . '%')
-                    ->orWhere('email', 'like', '%' . $search . '%');
+                $query->where('users.name', 'like', '%' . $search . '%')
+                    ->orWhere('users.email', 'like', '%' . $search . '%');
             })
             ->take(8)
             ->get();
