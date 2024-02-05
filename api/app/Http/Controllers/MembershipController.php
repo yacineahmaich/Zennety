7051e998c9f1216dbab38b56e8b060fe887c48f9
yacineahmaich <yacineahmaich@gmail.com>
2024-02-05 22:51:58 +0100

    feat [Backend]: authorize membership endpoints

--- api/app/Http/Controllers/MembershipController.php
+++ api/app/Http/Controllers/MembershipController.php
@@ -11,6 +11,12 @@ class MembershipController extends Controller
 {
     public function index(Request $request)
     {
+        $membership = Membership::fromRequest($request)
+            ->where('user_id', auth()->id())
+            ->first();
+            
+        $this->authorize('viewAny', [Membership::class, $membership->membershipable]);
+
         $role = $request->get('role');
         $search = $request->get('search');
 
@@ -26,6 +32,8 @@ class MembershipController extends Controller
 
     public function update(UpdateMembershipRequest $request, string $type, int $id, Membership $membership)
     {
+        $this->authorize('update', $membership);
+
         $membership->syncRoles([$request->validated('role')]);
 
         return response()->noContent();
@@ -33,6 +41,8 @@ class MembershipController extends Controller
 
     public function delete(string $type, int $id, Membership $membership)
     {
+        $this->authorize('delete', $membership);
+
         $membership->delete();
 
         return response()->noContent();
---
+++ api/app/Policies/MembershipPolicy.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Membership;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class MembershipPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user, $membershipable): bool
+    {
+        if (!$member = $user->memberFor($membershipable)) {
+            return false;
+        }
+
+        return $member->checkPermissionTo("view");
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Membership $membership): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Membership $membership): bool
+    {
+        if (!$member = $user->memberFor($membership->membershipable)) {
+            return false;
+        }
+
+        return $member->checkPermissionTo("update");
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Membership $membership): bool
+    {
+        if (!$member = $user->memberFor($membership->membershipable)) {
+            return false;
+        }
+
+        return $member->checkPermissionTo("delete");
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Membership $membership): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Membership $membership): bool
+    {
+        //
+    }
+}
