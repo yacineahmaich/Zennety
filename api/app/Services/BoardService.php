2347f428270e9e84834c9c47b0fe7d0ccdea9fe6
yacineahmaich <yacineahmaich@gmail.com>
2024-02-06 22:01:54 +0100

    fix [Backend]: add missing database transition in dependent database writes

--- api/app/Services/BoardService.php
+++ api/app/Services/BoardService.php
@@ -7,30 +7,37 @@ use App\DTO\BoardDTO;
 use App\Enums\Role;
 use App\Models\Board;
 use App\Models\Workspace;
+use Illuminate\Support\Facades\DB;
 
 class BoardService
 {
     public function store(BoardDTO $boardDTO, Workspace $workspace): Board
     {
-        $board = $workspace->boards()->create([
-            'name' => $boardDTO->name,
-            'description' => $boardDTO->description,
-            'visibility' => $boardDTO->visibility,
-        ]);
+        $board = DB::transaction(function () {
+            global $workspace, $boardDTO;
+            
+            $board = $workspace->boards()->create([
+                'name' => $boardDTO->name,
+                'description' => $boardDTO->description,
+                'visibility' => $boardDTO->visibility,
+            ]);
 
-        /**@var App\Models\Membership $owner */
-        $owner = $board->members()->create([
-            "user_id" => auth()->id()
-        ]);
+            /**@var App\Models\Membership $owner */
+            $owner = $board->members()->create([
+                "user_id" => auth()->id()
+            ]);
 
-        // Create some default statuses
-        $board->statuses()->createMany([
-            ["name" => "Pending"],
-            ["name" => "In Progress"],
-            ["name" => "Done"],
-        ]);
+            // Create some default statuses
+            $board->statuses()->createMany([
+                ["name" => "Pending"],
+                ["name" => "In Progress"],
+                ["name" => "Done"],
+            ]);
 
-        $owner->assignRole(Role::OWNER);
+            $owner->assignRole(Role::OWNER);
+
+            return $board;
+        });
 
         return $board;
     }
--- api/app/Services/WorkspaceService.php
+++ api/app/Services/WorkspaceService.php
@@ -7,6 +7,7 @@ use App\DTO\WorkspaceDTO;
 use App\Enums\Role;
 use App\Enums\Visibility;
 use App\Models\Workspace;
+use Illuminate\Support\Facades\DB;
 
 class WorkspaceService
 {
@@ -29,19 +30,24 @@ class WorkspaceService
 
     public function store(WorkspaceDTO $workspaceDTO): Workspace
     {
-        /**@var Workspace $workspace */
-        $workspace = Workspace::create([
-            'name' => $workspaceDTO->name,
-            'description' => $workspaceDTO->description,
-            'visibility' => $workspaceDTO->visibility,
-        ]);
+        $workspace = DB::transaction(function () {
+            global $workspaceDTO;
+            /**@var Workspace $workspace */
+            $workspace = Workspace::create([
+                'name' => $workspaceDTO->name,
+                'description' => $workspaceDTO->description,
+                'visibility' => $workspaceDTO->visibility,
+            ]);
 
-        /**@var App\Models\Membership $owner */
-        $owner = $workspace->members()->create([
-            "user_id" => auth()->id()
-        ]);
+            /**@var App\Models\Membership $owner */
+            $owner = $workspace->members()->create([
+                "user_id" => auth()->id()
+            ]);
 
-        $owner->assignRole(Role::OWNER);
+            $owner->assignRole(Role::OWNER);
+
+            return $workspace;
+        });
 
         return $workspace;
     }
