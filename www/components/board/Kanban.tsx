75ce3d945aed2b2e317ec01bab92a9a26a00ad25
yacineahmaich <yacineahmaich@gmail.com>
2024-02-16 22:24:26 +0100

    feat: implement the status column sorting ui

--- www/components/board/Kanban.tsx
+++ www/components/board/Kanban.tsx
@@ -1,29 +1,87 @@
 import { useStatuses } from "@/services";
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+} from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
+import { useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import { useMemo, useState } from "react";
 import CreateStatus from "./status/CreateStatus";
 import StatusColumn from "./status/StatusColumn";
 
 const Kanban = ({ board }: { board: App.Models.Board }) => {
+  const queryClient = useQueryClient();
   const router = useRouter();
+  const [active, setActive] = useState<App.Models.Status | null>(null);
   const { workspaceId, boardId } = router.query as {
     workspaceId: string;
     boardId: string;
   };
   const { statuses } = useStatuses(workspaceId, boardId);
 
+  const handleDragStart = (e: DragStartEvent) => {
+    if (e.active.data.current?.type === "status") {
+      setActive(e.active.data.current?.status);
+    }
+  };
+
+  const handleDragEnd = (e: DragEndEvent) => {
+    if (!e.over) return;
+
+    const activeStatusId = e.active.id;
+    const overStatusId = e.over.id;
+
+    if (activeStatusId === overStatusId) return;
+
+    queryClient.setQueryData<App.Models.Status[]>(
+      [
+        "workspaces",
+        board.workspaceId.toString(),
+        "boards",
+        board.id.toString(),
+        "statuses",
+      ],
+      (statuses = []) => {
+        const activeStatusIndex = statuses?.findIndex(
+          (s) => s.id == activeStatusId
+        );
+        const overStatusIndex = statuses?.findIndex(
+          (s) => s.id == overStatusId
+        );
+
+        // Hack to avoid delayed ui update
+        setActive(null);
+
+        return arrayMove(statuses, activeStatusIndex, overStatusIndex);
+      }
+    );
+  };
+
+  const items = useMemo(() => statuses?.map((status) => status.id), [statuses]);
+
   return (
-    <main className="flex-1 overflow-x-auto p-3">
-      <div className="flex h-full items-start gap-4">
-        {(statuses || []).length > 0 && (
-          <>
-            {statuses?.map((status) => (
-              <StatusColumn key={status.id} status={status} />
-            ))}
-            <CreateStatus board={board} />
-          </>
-        )}
-      </div>
-    </main>
+    <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+      <main className="flex-1 overflow-x-auto p-3">
+        <div className="flex h-full items-start gap-4">
+          {(statuses || []).length > 0 && (
+            <>
+              <SortableContext items={items || []}>
+                {statuses?.map((status) => (
+                  <StatusColumn key={status.id} status={status} />
+                ))}
+                <DragOverlay>
+                  {active && <StatusColumn status={active} />}
+                </DragOverlay>
+              </SortableContext>
+              <CreateStatus board={board} />
+            </>
+          )}
+        </div>
+      </main>
+    </DndContext>
   );
 };
 
--- www/components/board/status/StatusColumn.tsx
+++ www/components/board/status/StatusColumn.tsx
@@ -1,11 +1,52 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import StatusCard from "../card/StatusCard";
 import StatusColumnWrapper from "./StatusColumnWrapper";
 import StatusHeader from "./StatusHeader";
 
 const StatusColumn = ({ status }: { status: App.Models.Status }) => {
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: status.id,
+    data: {
+      type: "status",
+      status,
+    },
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="flex h-full w-72 flex-shrink-0 flex-col space-y-4"
+      >
+        <div className="h-9 rounded-lg bg-accent"></div>
+        <div className="h-full rounded-lg bg-accent"></div>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex h-full w-72 flex-col space-y-4">
-      <StatusHeader status={status} />
+    <div
+      ref={setNodeRef}
+      style={style}
+      className="flex h-full w-72 flex-col space-y-4"
+    >
+      <button {...attributes} {...listeners}>
+        <StatusHeader status={status} />
+      </button>
       <StatusColumnWrapper status={status}>
         {status?.cards?.map((card) => <StatusCard key={card.id} card={card} />)}
       </StatusColumnWrapper>
