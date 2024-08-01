2f56fac5826d5315000fb07adae60b944ddcafff
yacineahmaich <yacineahmaich@gmail.com>
2024-08-01 01:21:25 +0100

    feat: append participants to card resource

--- api/app/Http/Resources/CardResource.php
+++ api/app/Http/Resources/CardResource.php
@@ -15,14 +15,26 @@ class CardResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $activities = Activity::whereMorphedTo('subject', $this)
+            ->with("causer")
+            ->get();
+
+        $participants = [];
+
+        foreach($activities as $activity) {
+            if(!in_array($activity->causer->id, array_keys($participants))) {
+                $participants[$activity->causer->id] = $activity->causer;
+            }
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
             'statusId' => $this->status_id,
-            'activities' => Activity::whereMorphedTo('subject', $this)
-                ->with("causer")
-                ->get()
+            'activities' => $activities,
+            'participants' => array_values($participants),
+            'updatedAt' => $this->updated_at
         ];
     }
 }
