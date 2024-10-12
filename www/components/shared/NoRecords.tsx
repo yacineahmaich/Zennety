14795655b0ffaf43dda8c5e5f39aa7f161731467
yacineahmaich <yacineahmaich@gmail.com>
2024-10-12 23:58:42 +0100

    chore: add missing translation

--- www/components/shared/NoRecords.tsx
+++ www/components/shared/NoRecords.tsx
@@ -1,10 +1,13 @@
-import { AudioLinesIcon } from "lucide-react";
+import { PackageIcon } from "lucide-react";
+import { useTranslation } from "next-i18next";
 
-const NoRecords = ({ size = "lg" }: { size: "sm" | "md" | "lg" }) => {
+const NoRecords = () => {
+  const { t } = useTranslation("common");
+
   return (
     <div className={"flex flex-col items-center gap-3 py-20"}>
-      <AudioLinesIcon size={50} className="text-muted-foreground" />
-      <span className="text-muted-foreground">No records found.</span>
+      <PackageIcon size={50} className="stroke-1 text-muted-foreground" />
+      <span className="text-muted-foreground">{t("no-records")}</span>
     </div>
   );
 };
