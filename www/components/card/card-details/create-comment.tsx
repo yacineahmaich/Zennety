5256c2476e5ac6e0a885565bd06d4058903a5301
yacineahmaich <yacineahmaich@gmail.com>
2025-01-11 12:39:04 +0100

    fix: create comment form

--- www/components/card/card-details/create-comment.tsx
+++ www/components/card/card-details/create-comment.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Textarea } from "@/components/ui/textarea";
 import { useCreateCardComment } from "@/services/card";
 import { IBoard, ICard, IStatus } from "@/types/models";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { SendIcon } from "lucide-react";
 import { useTranslation } from "next-i18next";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 type Props = {
   status: IStatus;
@@ -12,49 +16,70 @@ type Props = {
   card: ICard;
 };
 
+const formSchema = z.object({
+  workspaceId: z.number(),
+  boardId: z.number(),
+  statusId: z.number(),
+  cardId: z.number(),
+
+  comment: z.string(),
+});
+
+export type CreateComment = z.infer<typeof formSchema>;
+
 const CreateComment = ({ board, status, card }: Props) => {
+  const router = useRouter();
   const { t } = useTranslation("common");
+  const { createCardComment, isLoading } = useCreateCardComment();
 
-  const [content, setContent] = useState("");
+  const { workspaceId, boardId } = router.query as {
+    workspaceId: string;
+    boardId: string;
+  };
 
-  const { createCardComment, isLoading: isLoading } = useCreateCardComment();
+  const form = useForm<CreateComment>({
+    defaultValues: {
+      workspaceId: +workspaceId,
+      boardId: +boardId,
+      statusId: status.id,
+      cardId: card.id,
+    },
+    resolver: zodResolver(formSchema),
+  });
 
-  const handleSendComment = () => {
-    createCardComment(
-      {
-        workspaceId: board.workspaceId,
-        boardId: board.id,
-        statusId: status.id,
-        cardId: card.id,
-        comment: content,
+  const onSubmit = (values: CreateComment) => {
+    console.log(values);
+    createCardComment(values, {
+      onSuccess() {
+        form.reset((df) => ({ ...df, comment: "" }));
       },
-      {
-        onSuccess() {
-          setContent("");
-        },
-      }
-    );
+    });
   };
 
   return (
     <div className="-ml-5 border-t">
-      <div className="flex w-full gap-2 p-3 pb-0">
-        <Textarea
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          placeholder={t("share-thoughts")}
-        />
-        <div>
-          <Button
-            size="sm"
-            className="h-full"
-            onClick={() => handleSendComment()}
-            disabled={isLoading}
-          >
-            <SendIcon size={18} />
-          </Button>
-        </div>
-      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="flex w-full gap-2 p-3 pb-0">
+            <FormField
+              control={form.control}
+              name="comment"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormControl>
+                    <Textarea placeholder={t("share-thoughts")} {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <div>
+              <Button size="sm" className="h-full" disabled={isLoading}>
+                <SendIcon size={18} />
+              </Button>
+            </div>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 };
