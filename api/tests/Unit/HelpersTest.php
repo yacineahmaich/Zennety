fe7eb47c04310a0230774946f0f132af531878f9
yacineahmaich <yacineahmaich@gmail.com>
2025-04-22 21:35:00 +0100

    test: group helpers tests

--- api/tests/Unit/HelpersTest.php
+++ api/tests/Unit/HelpersTest.php
@@ -4,21 +4,19 @@ namespace Tests\Unit;
 
 use App\Models\Workspace;
 
-test('getModelNamespace returns correct namespace', function () {
-    expect(getModelNamespace('workspace'))->toBe(Workspace::class);
+describe('getModelNamespace', function () {
+    test('returns correct namespace', function () {
+        expect(getModelNamespace('workspace'))->toBe(Workspace::class);
+    });
+    
+    test('returns null for non existing model', function () {
+        expect(getModelNamespace('unknown'))->toBe(null);
+    });    
 });
 
-test('getModelNamespace returns null for non existing model', function () {
-    expect(getModelNamespace('unknown'))->toBe(null);
+describe('getModelPrefix', function () {
+    test('returns expected prefix', function () {
+        expect(getModelPrefix('workspace'))->toBe('w/')
+            ->and(getModelPrefix('board'))->toBe('b/');
+    });   
 });
-
-test('getModelPrefix returns expected prefix', function () {
-    expect(getModelPrefix('workspace'))->toBe('w/')
-        ->and(getModelPrefix('board'))->toBe('b/');
-});
-
-//test('getModel returns expected model', function() {
-//   $user = User::factory()->create();
-//
-//   expect(getModel('user', $user->id))->toBe($user);
-//});
