bda7659d033d089e9bc67226f894ebf816e846b6
yacineahmaich <yacineahmaich@gmail.com>
2025-05-11 20:06:33 +0100

    fix: deploy script link storage

--- api/scripts/00-laravel-deploy.sh
+++ api/scripts/00-laravel-deploy.sh
@@ -3,8 +3,8 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
+echo "linking storage..."
+php artisan storage:link --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -13,4 +13,4 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate
