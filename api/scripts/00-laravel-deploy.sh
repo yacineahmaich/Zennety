fffb5a42f2d8758d78ea3aac32e9197b898a8e69
yacineahmaich <yacineahmaich@gmail.com>
2025-05-13 19:57:17 +0100

    fix: storage && cache propriate permissions

--- api/scripts/00-laravel-deploy.sh
+++ api/scripts/00-laravel-deploy.sh
@@ -3,6 +3,16 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+# Give permissions to storage and cache
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+# Create SQLite file if it doesn't exist
+if [ ! -f /var/www/html/database/database.sqlite ]; then
+    touch /var/www/html/database/database.sqlite
+    chmod 666 /var/www/html/database/database.sqlite
+fi
+
 echo "linking storage..."
 php artisan storage:link
 
