d7d4189518894d3b61c887520c5c32fee09f646b
yacineahmaich <yacineahmaich@gmail.com>
2025-05-19 22:10:55 +0100

    chore: update deploy script

--- api/scripts/00-laravel-deploy.sh
+++ api/scripts/00-laravel-deploy.sh
@@ -3,16 +3,6 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-# Give permissions to storage and cache
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
-
-# Create SQLite file if it doesn't exist
-if [ ! -f /data/database.sqlite ]; then
-    touch /data/database.sqlite
-    chmod 666 /data/database.sqlite
-fi
-
 echo "linking storage..."
 php artisan storage:link
 
